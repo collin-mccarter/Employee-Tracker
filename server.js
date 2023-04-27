14016aefe952caddbea8d30504af28d22cfa04a5
collin-mccarter <collinmccarter12@gmail.com>
2023-04-26 22:44:29 -0500

    updated server

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ connection.connect(function (err) {
            ║║      ╔═╝║                     ╔═╝║
            ╚╝      ╚══╝                     ╚══╝`)
 
-    init();
+    menuQuestions();
 })
 
-function init() {
+function menuQuestions() {
     inquirer.prompt({
         type:"list",
         name:"menu",
@@ -53,5 +53,92 @@ function init() {
             "Quit"
         ]
     })
+
+    .then(function ({menu}) {
+        if (answer.choices === "View All Employees") {
+            ViewAllEmployees();
+        }
+        else if (answer.choices === "Add Employee") {
+            AddEmployee();
+        } 
+        else if (answer.choices === "Update Employee Role") {
+            UpdateEmployeeRole();
+        }
+        else if (answer.choices === "View All Roles") {
+            ViewAllRoles();
+        }
+        else if (answer.choices === "Add Role") {
+            AddRole();
+        }
+        else if (answer.choices === "View All Departments") {
+            ViewAllDepartments();
+        }
+        else if (answer.choices === "Add Department") {
+            AddDepartment();
+        }
+        else if (answer.choices === "Quit") {
+            console.log("Thank you for using employee tracker!");
+            connection.end();
+        }
+    })
 }
 
+function ViewAllEmployees() {
+    console.log("Viewing all employees: \n")
+
+    let query = "SELECT * FROM employee";
+
+    connection.query(query, function(err,res) {
+    if (err) throw err;
+
+    console.table(res);
+    
+    menuQuestions();
+    })
+}
+
+function AddEmployee() {
+    console.log("Please enter employee information: \n")
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the employees first name?",
+            name: "first_name"
+        },
+        {
+            type: "input",
+            message: "What is the employees last name?",
+            name: "last_name"
+        },
+        {
+            type: "list",
+            message: "What is the employees role ID number??",
+            name: "roleId"
+        },
+        {
+            type: "input",
+            message: "What is the manager ID number?",
+            name: "managerId"
+        }
+    ])
+    .then(function(answer) {
+      connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [answer.first_name, answer.last_name, answer.roleId, answer.managerId], function(err, res) {
+        if (err) throw err;
+
+        console.table(res);
+
+        menuQuestions();
+      });
+    });
+}
+
+function UpdateEmployeeRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Which employee would you like to update?",
+            name: ""
+        }
+    ])
+}
