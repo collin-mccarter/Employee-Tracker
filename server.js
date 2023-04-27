4f96aadbb5c48d01617ed6ad6e0e8a12e9c90efc
collin-mccarter <collinmccarter12@gmail.com>
2023-04-26 21:37:21 -0500

    started server.js

---
+++ server.js
@@ -0,0 +1,57 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+    host: '127.0.0.1',
+ 
+    user: 'root',
+
+    password: '',
+
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employee database.`)
+);
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`
+    ╔═══╗     ╔╗              ╔═╗╔═╗
+    ║╔══╝     ║║              ║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+           ║║      ╔═╝║                     ╔═╝║
+           ╚╝      ╚══╝                     ╚══╝`)
+
+    init();
+})
+
+function init() {
+    inquirer.prompt({
+        type:"list",
+        name:"menu",
+        message:"What Would You Like To Do?",
+        choices:[
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Quit"
+        ]
+    })
+}
+
